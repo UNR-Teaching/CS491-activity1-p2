1f859255f3c100c4149861ddbf9b7f8ba69ea07d
jayden williams <jay.i.will2024@gmail.com>
2022-01-25 14:30:03 -0800

    HW1Final

--- Test.py
+++ Test.py
@@ -1,11 +1,39 @@
-from curses import ACS_GEQUAL
 import Car_Bike as cb
+import sys
+#def Main():
+    #test_command = ''
+    #while test_command != 'f':
+        #test_command = input
+    #ans = True
+def main():
+   menu()
+   pass
 
-def Main():
-    test_command = ''
-    while test_command != 'f':
-        test_command = input
+def menu():
+    print("************Welcome to The Garage**************")
+    print()
+    menuChoice = input("""
+        1: Car
+        2: Bike
+        3: Exit
+        Select The Car Type: """)
 
+    if menuChoice == "1":
+        Car()
+    elif menuChoice == "2":
+        Bike()
+    elif menuChoice=="3":
+        sys.exit
+    else:
+        print("You've selected an invalild type")
+        print("Please try again")
+        menu()
+
+def Car(cb):
+   pass
+    
+def Bike():
+   pass
 
 if __name__ == '__main__':
     cb.main()

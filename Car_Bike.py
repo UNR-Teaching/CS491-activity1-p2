05a0b9106b367ef73833405d6b894b8b8b27c735
jayden williams <jay.i.will2024@gmail.com>
2022-01-24 18:20:31 -0800

    HW1Final

---
+++ Car_Bike.py
@@ -0,0 +1,38 @@
+from curses import ACS_GEQUAL
+
+class Vehicle:
+    def __init__(Vehicle, color, age):
+        Vehicle.color = color
+        Vehicle.age = age
+        Vehicle.weight = weight
+        def start(Vehicle):
+            print("Starting Up...")
+        def drive(Vehicle):
+            print("Zoom!...")
+        def brake(Vehicle):
+            print("Pumping The Brakes!...")
+        def gas(Vehicle):
+            print("Stepping on the Gas!...")
+        pass
+
+class Car(Vehicle):
+    def __init__(Car, make, name, owner):
+        Car.name = name
+        Car.make = make
+        set.owner = "Wayne"
+        set.name = "Pinto"
+        set.make = "Ford"
+        pass
+
+class Bike(Vehicle):
+    def __init__(Bike, make, name):
+        Bike.name = name
+        Bike.make = make
+        set.owner = "Garth"
+        set.make = "BMX"
+
+    def pedal(Vehicle):
+        print("Pushing P....")
+        pass
+
+
---
+++ Test.py
@@ -0,0 +1,11 @@
+from curses import ACS_GEQUAL
+import Car_Bike as cb
+
+def Main():
+    test_command = ''
+    while test_command != 'f'
+        test_command = input
+
+
+if __name__ == '__main__':
+    cb.main()
